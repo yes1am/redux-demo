2ce6065d9a6d720f76db5cadc3f8ce4c80bbf16e
Jianping Song <jianping.song@shopee.com>
2020-02-12 15:21:02 +0800

    feat: add redux-logger middleware

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 ## redux-demo
 
+## 开发
+
+`npm install`  
+
+`npm start`
+
 ## 调试插件
 
 - React Develop Tools
 - Redux DevTools
 
-Redux DevTools 设置:  
-
+Redux DevTools [文档](https://github.com/zalmoxisus/redux-devtools-extension)
+设置:  
 ```js
  const store = createStore(
    reducer,
--- app/views/index.js
+++ app/views/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { createLogger } from 'redux-logger'
 import App from './app'
+const logger = createLogger()
 
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(logger)
+))
 
 ReactDOM.render(
   <Provider store={store}>
